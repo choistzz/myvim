53d9178adaa701c00917245d0a065e81aa609957
choi <809846433@qq.com>
2024-04-14 00:55:49 +0800

    20240414

---
+++ lua/options.lua
@@ -0,0 +1,9 @@
+-- 显示行号
+vim.opt.ignorecase = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.mouse:append('a')
+vim.opt.autoindent = true
